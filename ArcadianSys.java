14d048975c5ce4bbc5999d2f90c84427a153b399
NateAnderson500 <nja5387@gmail.com>
2022-04-12 16:08:50 -0400

    created file and added base code

---
+++ ArcadianSys.java
@@ -0,0 +1,18 @@
+
+/**
+ * File that holds the array that contains the entire Arcadian System
+ * 
+ * Author: Nate Anderson
+ * Version: Alpha Build 0.1
+ */
+
+ //creates main method
+    public class ArcadianSys {
+        //creates array
+        public static String[][] arcadian = new String[100][100];
+        
+        //creates main method
+        public static void main(String[] args) {
+            
+        }
+    }
