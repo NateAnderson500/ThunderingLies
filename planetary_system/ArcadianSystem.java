c6bfb9428eaa131e211b72ebeb1224c3ec64871e
Paul B <90632525+PaulBrouillette@users.noreply.github.com>
2022-04-18 18:05:17 -0400

    Planet objects and setting up the array of Planets

---
+++ planetary_system/ArcadianSystem.java
@@ -0,0 +1,82 @@
+package planetary_system;
+
+public class ArcadianSystem {
+   //2d array goes row (top to bottom), column (left to right)
+    public Planet[][] planets;
+    public Planet corvus, thantos, newTerra, ligo, 
+        labyrinth, keres, aesop, uncas, abenaki, pollux, 
+        boreas, macintyre, vanu, gandash, artemisia;
+
+    /**
+     * Constructor for ArcadianSystem. Makes every space in the array list either a space that represents
+     * a place in space, or a planet.
+     */ 
+    public ArcadianSystem() {
+        planets = new Planet[16][5];
+
+        //Fill every index of planets with a planet named space
+        for (int i = 0; i < planets.length; i++) {
+            for (int j = 0; j < planets[i].length; j++) {
+                planets[i][j] = new Planet("Space", "Nothing here, just space.", new String[]{}, false);
+            }
+        }
+
+        corvus = new Planet("Corvus", "A blisteringly hot planet, it is closest to the sun.", new String[]{"Thantos"}, true);
+        planets[0][0] = corvus;
+
+        thantos = new Planet("Thantos", "A hot micro planet, all life can exist only underground.", new String[]{"Corvus", "New Terra"}, true);
+        planets[1][0] = thantos;
+
+        newTerra = new Planet("New Terra", "An tropical planet, it is known for its plant and animal exports.", new String[]{"Thantos", "Ligo"}, true);
+        planets[2][0] = newTerra;
+
+        ligo = new Planet("Ligo", "A moderate planet, its poles are icy and frigid.", new String[]{"New Terra", "Labyrinth"}, true);
+        planets[3][0] = ligo;
+
+        labyrinth = new Planet("Labyrinth", "A cooler planet, its maze-like caverns are home to vast quantities of metals.", new String[]{"Ligo", "Keres"}, true);
+        planets[4][0] = labyrinth;
+
+        keres = new Planet("Keres", "A desert planet, scarce resources exist on the barren land.", new String[]{"Labyrinth", "Aesop"}, true);
+        planets[5][0] = keres;
+
+        aesop = new Planet("Aesop", "A micro planet, the large crater on the planet contains the only life of the planet.", new String[]{"Keres", "Uncas"}, true);
+        planets[6][0] = aesop;
+
+        uncas = new Planet("Uncas", "A cooler planet, its two large rings are consistently harvested for their ice.", new String[]{"Aesop", "Abenaki"}, true);
+        planets[7][0] = uncas;
+
+        abenaki = new Planet("Abenaki", "A volcanic planet, it is totally uninhabitable and the warmest of the outer planets.", new String[]{"Uncas", "Pollux"}, true);
+        planets[8][0] = abenaki;
+
+        pollux = new Planet("Pollux", "An actic planet, it is heavily populated despite its temperature.", new String[]{"Abenaki", "Boreas"}, true);
+        planets[9][0] = pollux;
+
+        boreas = new Planet("Boreas", "An icy planet, the surface is completely covered with ice and snow.", new String[]{"Pollux", "Macintyre"}, true);
+        planets[10][0] = boreas;
+
+        macintyre = new Planet("Macintyre", "A frigid planet, it is a literal ball of ice.", new String[]{"Boreas", "Vanu"}, true);
+        planets[11][0] = macintyre;
+
+        vanu = new Planet("Vanu", "A bitter planet, a strange mineral underground gives the planet a purple hue.", new String[]{"Macintyre", "Gandash"}, true);
+        planets[12][0] = vanu;
+
+        gandash = new Planet("Gandash", "A metal planet, its poisonous metal gas rings are mined for its resources.", new String[]{"Vanu", "Artmeisia"}, true);
+        planets[13][0] = gandash;
+
+        artemisia = new Planet("Artemisia", "A inhospitable planet, its strange climate can vary from below freezing to temperatures similar to the sun.", new String[]{"Gandash"}, true);
+        planets[14][0] = artemisia;
+    }
+
+    /**
+     * Prints the names, descriptions, and adjacent planets of every planet in the system.
+     */
+    public void printPlanets() {
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (!planets[i][j].name.equals("Space")) {
+                    System.out.println(planets[i][j].name + ": " + planets[i][0].description + " Next to " + planets[i][0].getAdjacentPlanets());
+                }
+            }
+        }
+    }
+}
---
+++ planetary_system/Planet.java
@@ -0,0 +1,88 @@
+package planetary_system;
+
+public class Planet {
+    String name;
+    String description;
+    String[] adjacentPlanets;
+    Boolean occupied;   
+
+    /**
+     * Constructor for Planet.
+     * 
+     * @param name The name of the planet.
+     * @param description The description of the planet.
+     * @param adjacentPlanets The names of the planets that are adjacent to this planet.
+     * @param occupied Whether or not the planet is occupied, if a planet is not "Space", this is 
+     * true
+     */
+    public Planet(String name, String description, String[] adjacentPlanets, Boolean occupied) {
+        this.name = name;
+        this.description = description;
+        this.adjacentPlanets = adjacentPlanets;
+        this.occupied = occupied;
+    }
+
+    /**
+     * toString method for displaying the name, description, and adjacent planets of a planet.
+     * 
+     * @return The name, description, and adjacent planets of a planet.
+     */
+    @Override
+    public String toString() {
+        return name + ": " + description + " Next to " + getAdjacentPlanets();
+    }
+
+    /**
+     * This method returns the name of the planet.
+     * 
+     * @return The name of the planet.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * This method returns the description of the planet.
+     * 
+     * @return The description of the planet.
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * This method returns whether or not the planet is occupied.
+     * 
+     * @return Whether or not the planet is occupied.
+     */
+    public Boolean getOccupied() {
+        return occupied;
+    }
+
+    /**
+     * This method returns the names of the planets that are adjacent to this planet.
+     * 
+     * @return The names of the planets that are adjacent to this planet.
+     */
+    public String getAdjacentPlanets() {
+        String s = "";
+        if (adjacentPlanets.length == 1) {
+            s += adjacentPlanets[0];
+            
+        }
+        else {
+            s += adjacentPlanets[0] + " and " + adjacentPlanets[1];
+        }
+        return s;
+    }
+
+    /**
+     * This method gets the location of the .png file for the planet in the
+     * assets/planets folder to assist with displaying the planet on the UI.
+     * 
+     * @return The location of the .png file for the planet in the assets/planets folder.
+     */
+    public String getPlanetPicture() {
+        return "assets/planets/" + name + ".png";
+    }
+}
