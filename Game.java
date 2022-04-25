f8db46bcb588b9f17305de01eab3c2b75ce2704c
PaulBrouillette <pb30brou@siena.edu>
2022-04-25 10:00:41 -0400

    Makes new saveGame object

--- Game.java
+++ Game.java
@@ -1,5 +1,4 @@
 import javax.imageio.ImageIO;
-import javax.swing.ImageIcon;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import inventory_system.Inventory;
@@ -9,7 +8,9 @@ import planetary_system.Planet;
 import java.awt.image.BufferedImage;
 import java.awt.Image;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 /**
  * The main file where everything for the game is called and managed here. 
@@ -20,6 +21,8 @@ public class Game implements Runnable {
     public static Planet currentPlanet;
     public static Inventory inventory = new Inventory();
     public static Ship currentShip;
+    public static saveGameData saveGame;
+    Scanner sc;
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Game());
     }
@@ -42,10 +45,20 @@ public class Game implements Runnable {
         }
         Image image = bi.getScaledInstance(350, 350, Image.SCALE_SMOOTH);
 
+        File text = new File("savegame.txt");
+        try {
+            sc = new Scanner(text);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String planetString = sc.nextLine();
+
         currentShip = new Ship("Le Ship", "This is le ship hehe", true, 2100, 100, 69, image);
         inventory.addItem(new Item("Wrench", 1, 45));
         arcadianSystem = new ArcadianSystem();
-        currentPlanet = arcadianSystem.vanu;
+        currentPlanet = arcadianSystem.getPlanet(planetString);
+        saveGame = new saveGameData();
         ui = new UserInterface();
         ui.run();
     }
